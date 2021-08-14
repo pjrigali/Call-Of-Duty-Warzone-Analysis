602f392fc76a90ffbd463fe31ea73219dbb0caa2
Peter <prigali>
2021-08-13 21:52:28 -0500

    adds data filter function

--- Utils/base.py
+++ Utils/base.py
@@ -1,7 +1,9 @@
+import pandas as pd
 import numpy as np
 
 
-def normalize(arr: np.ndarray, multi: bool = False) -> np.ndarray:
+def normalize(arr: np.ndarray,
+              multi: bool = False) -> np.ndarray:
     if multi:
         return np.array([(arr[:, i] - np.min(arr[:, i])) / (np.max(arr[:, i]) - np.min(arr[:, i])) for i in
                          range(arr.shape[1])]).T
@@ -9,7 +11,8 @@ def normalize(arr: np.ndarray, multi: bool = False) -> np.ndarray:
         return np.around((arr - np.min(arr)) / (np.max(arr) - np.min(arr)).T, 3)
 
 
-def running_mean(arr: np.ndarray, num: int) -> np.ndarray:
+def running_mean(arr: np.ndarray,
+                 num: int) -> np.ndarray:
     cum_sum = np.cumsum(np.insert(arr=arr, values=[np.mean(arr)] * num, obj=0))
     return (cum_sum[num:] - cum_sum[:-num]) / float(num)
 
@@ -19,3 +22,25 @@ def cum_mean(arr: np.ndarray) -> np.ndarray:
     for i in range(cum_sum.shape[0]):
         cum_sum[i] = cum_sum[i] / (i + 1)
     return cum_sum
+
+
+def filter_df(data: pd.DataFrame,
+              _map: str = None,
+              _mode: str = None,
+              _uno: int = None,
+              username_dic: dict = None,
+              username: str = None) -> pd.DataFrame:
+
+    if _map:
+        data = data[data['map'] == _map]
+
+    if _mode:
+        data = data[data['mode'] == _mode]
+
+    if username:
+        data = data[data['uno'] == username_dic[username]]
+
+    if _uno:
+        data = data[data['uno'] == _uno]
+
+    return data.reset_index(drop=True).sort_values('startDateTime', ascending=True)
