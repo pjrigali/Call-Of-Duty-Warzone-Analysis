ba463acbf11bbd38d34ba2eb667ec3631e01f047
Peter <prigali>
2021-08-15 00:56:53 -0500

    Revised default regression calc

--- Utils/regress.py
+++ Utils/regress.py
@@ -7,89 +7,153 @@ import matplotlib.pyplot as plt
 pd.set_option('display.max_columns', None)
 
 
+# def regression_calcs(df: pd.DataFrame,
+#                      y: np.ndarray,
+#                      r2: float = None,
+#                      constant_coef: float = None,
+#                      variable_coef: float = None,
+#                      confidence_interval_lower: float = None,
+#                      confidence_interval_higher: float = None,
+#                      p_value: float = 0.05,
+#                      sort_value: list = None,
+#                      filtering: bool = False,
+#                      ):
+#     if sort_value is None:
+#         sort_value = ['P Value', True]
+#
+#     final_cols = []
+#     if r2 is not None:
+#         final_cols.append('R Squared')
+#
+#     if constant_coef is not None:
+#         final_cols.append('Constant Coefficient')
+#
+#     if variable_coef is not None:
+#         final_cols.append('Word Coefficient')
+#
+#     if confidence_interval_lower is not None:
+#         final_cols.append('Lower Confidence Interval')
+#
+#     if confidence_interval_higher is not None:
+#         final_cols.append('Higher Confidence Interval')
+#
+#     if p_value is not None:
+#         final_cols.append('P Value')
+#
+#     col_lst = list(df.columns)
+#     result = {}
+#     for word in col_lst:
+#         x = add_constant(np.array(df[word], dtype=float))
+#         model = regression.linear_model.OLS(y, x).fit()
+#
+#         model_results = []
+#         if r2 is not None:
+#             model_results.append(model.rsquared)
+#
+#         if constant_coef is not None:
+#             model_results.append(model.params[0])
+#
+#         if variable_coef is not None:
+#             model_results.append(model.params[1])
+#
+#         if confidence_interval_lower is not None:
+#             model_results.append(model.conf_int()[1, :2][0])
+#
+#         if confidence_interval_higher is not None:
+#             model_results.append(model.conf_int()[1, :2][1])
+#
+#         if p_value is not None:
+#             model_results.append(model.pvalues[1])
+#
+#         result[word] = model_results
+#
+#     result_df = pd.DataFrame.from_dict(result, orient='index', columns=final_cols)
+#     if filtering:
+#         if r2 is not None:
+#             result_df = result_df[result_df['R Squared'] > r2]
+#
+#         if constant_coef is not None:
+#             result_df = result_df[result_df['Constant Coefficient'] > constant_coef]
+#
+#         if variable_coef is not None:
+#             result_df = result_df[result_df['Word Coefficient'] > variable_coef]
+#
+#         if confidence_interval_lower is not None:
+#             result_df = result_df[result_df['Lower Confidence Interval'] > confidence_interval_lower]
+#
+#         if confidence_interval_higher is not None:
+#             result_df = result_df[result_df['Higher Confidence Interval'] > confidence_interval_higher]
+#
+#         if p_value is not None:
+#             result_df = result_df[result_df['P Value'] < p_value]
+#
+#     final = result_df.sort_values(sort_value[0], ascending=sort_value[1]).round(3)
+#
+#     return final, list(final.index)
+
+
 def regression_calcs(df: pd.DataFrame,
                      y: np.ndarray,
                      r2: float = None,
                      constant_coef: float = None,
-                     variable_coef: float = None,
+                     word_coef: float = None,
                      confidence_interval_lower: float = None,
                      confidence_interval_higher: float = None,
                      p_value: float = 0.05,
-                     sort_value: list = None,
-                     filtering: bool = False,
-                     ):
-    if sort_value is None:
-        sort_value = ['P Value', True]
-    
+                     sort_value=['P Value', True],
+                     ) -> pd.DataFrame:
     final_cols = []
     if r2 is not None:
         final_cols.append('R Squared')
-    
     if constant_coef is not None:
         final_cols.append('Constant Coefficient')
-    
-    if variable_coef is not None:
+    if word_coef is not None:
         final_cols.append('Word Coefficient')
-    
     if confidence_interval_lower is not None:
         final_cols.append('Lower Confidence Interval')
-    
     if confidence_interval_higher is not None:
         final_cols.append('Higher Confidence Interval')
-    
     if p_value is not None:
         final_cols.append('P Value')
-    
+
     col_lst = list(df.columns)
     result = {}
     for word in col_lst:
-        x = add_constant(np.array(df[word], dtype=float))
+        x = stats.tools.add_constant(np.array(df[word], dtype=float))
         model = regression.linear_model.OLS(y, x).fit()
-        
+
         model_results = []
         if r2 is not None:
             model_results.append(model.rsquared)
-        
         if constant_coef is not None:
             model_results.append(model.params[0])
-        
-        if variable_coef is not None:
+        if word_coef is not None:
             model_results.append(model.params[1])
-        
         if confidence_interval_lower is not None:
             model_results.append(model.conf_int()[1, :2][0])
-        
         if confidence_interval_higher is not None:
             model_results.append(model.conf_int()[1, :2][1])
-        
         if p_value is not None:
             model_results.append(model.pvalues[1])
-        
         result[word] = model_results
-    
     result_df = pd.DataFrame.from_dict(result, orient='index', columns=final_cols)
-    if filtering:
-        if r2 is not None:
-            result_df = result_df[result_df['R Squared'] > r2]
-        
-        if constant_coef is not None:
-            result_df = result_df[result_df['Constant Coefficient'] > constant_coef]
-        
-        if variable_coef is not None:
-            result_df = result_df[result_df['Word Coefficient'] > variable_coef]
-        
-        if confidence_interval_lower is not None:
-            result_df = result_df[result_df['Lower Confidence Interval'] > confidence_interval_lower]
-        
-        if confidence_interval_higher is not None:
-            result_df = result_df[result_df['Higher Confidence Interval'] > confidence_interval_higher]
-        
-        if p_value is not None:
-            result_df = result_df[result_df['P Value'] < p_value]
-    
+
+    if r2 is not None:
+        result_df = result_df[result_df['R Squared'] > r2]
+    # if constant_coef is not None:
+    #     result_df = result_df[result_df['Constant Coefficient'] > constant_coef]
+    # if word_coef is not None:
+    #     result_df = result_df[result_df['Word Coefficient'] > word_coef]
+    if confidence_interval_lower is not None:
+        result_df = result_df[result_df['Lower Confidence Interval'] > confidence_interval_lower]
+    # if confidence_interval_higher is not None:
+    #     result_df = result_df[result_df['Higher Confidence Interval'] > confidence_interval_higher]
+    if p_value is not None:
+        result_df = result_df[result_df['P Value'] < p_value]
     final = result_df.sort_values(sort_value[0], ascending=sort_value[1]).round(3)
-    
-    return final, list(final.index)
+
+    return final
+
 
 def regress(xtrain,
             ytrain,
