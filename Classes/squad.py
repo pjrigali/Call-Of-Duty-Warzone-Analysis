4e51484c9ea076385837c6632c26c29470abb7d1
Peter <prigali>
2021-08-20 14:15:17 -0500

    updates to Performance, Person, and Squad classes

--- Classes/squad.py
+++ Classes/squad.py
@@ -1,131 +1,166 @@
-from typing import List, Optional
+from typing import List, Optional, Dict
 import pandas as pd
 import numpy as np
 from Classes.document_filter import DocumentFilter
 
+mu_lst = ['headshots', 'kills', 'deaths', 'kdRatio', 'longestStreak', 'scorePerMinute', 'distanceTraveled',
+          'percentTimeMoving', 'damageDone', 'damageTaken', 'missionsComplete', 'timePlayed', 'objectiveBrCacheOpen',
+          'objectiveBrKioskBuy', 'objectiveBrMissionPickupTablet', 'objectiveLastStandKill', 'objectiveReviver',
+          'objectiveTeamWiped', 'objectiveLastStandKill', 'objectiveBrDownEnemyCircle1',
+          'objectiveBrDownEnemyCircle2', 'objectiveBrDownEnemyCircle3', 'objectiveBrDownEnemyCircle4',
+          'objectiveBrDownEnemyCircle5', 'objectiveBrDownEnemyCircle6', 'placementPercent', 'headshotRatio']
+
+sum_lst = ['headshots', 'kills', 'deaths', 'distanceTraveled', 'damageDone', 'damageTaken', 'missionsComplete',
+           'timePlayed', 'objectiveBrCacheOpen', 'objectiveBrKioskBuy', 'objectiveBrMissionPickupTablet',
+           'objectiveLastStandKill', 'objectiveReviver', 'objectiveTeamWiped', 'objectiveLastStandKill',
+           'objectiveBrDownEnemyCircle1', 'objectiveBrDownEnemyCircle2', 'objectiveBrDownEnemyCircle3',
+           'objectiveBrDownEnemyCircle4', 'objectiveBrDownEnemyCircle5', 'objectiveBrDownEnemyCircle6']
+
+max_lst = ['headshots', 'kills', 'deaths', 'distanceTraveled', 'damageDone', 'damageTaken', 'missionsComplete',
+           'timePlayed', 'objectiveBrCacheOpen', 'objectiveBrKioskBuy', 'objectiveBrMissionPickupTablet',
+           'objectiveLastStandKill', 'objectiveReviver', 'objectiveTeamWiped', 'objectiveLastStandKill',
+           'objectiveBrDownEnemyCircle1', 'objectiveBrDownEnemyCircle2', 'objectiveBrDownEnemyCircle3',
+           'objectiveBrDownEnemyCircle4', 'objectiveBrDownEnemyCircle5', 'objectiveBrDownEnemyCircle6', 'kdRatio',
+           'headshotRatio']
+
+lst_dic = {'mu': mu_lst, 'sum': sum_lst, 'max': max_lst}
+
+
+def _get_stats(doc_filter: DocumentFilter) -> Dict[str, float]:
+    df = doc_filter.df.fillna(0)
+    stats_dic = {'game_count': len(df), 'win_count': len(df[df['teamPlacement'] == 1]), 'win_percent': 0.0}
+    if stats_dic['win_count'] != 0:
+        stats_dic['win_percent'] = stats_dic['win_count'] / stats_dic['game_count']
+
+    if doc_filter.map_choice == 'mp_d':
+        stats_dic['top_10_count'] = len(df[(df['teamPlacement'] <= 10) & (df['teamPlacement'] > 1)])
+        stats_dic['top_10_percent'] = 0.0
+        if stats_dic['top_10_count'] != 0:
+            stats_dic['top_10_percent'] = stats_dic['top_10_count'] / stats_dic['game_count']
+    else:
+        stats_dic['top_5_count'] = len(df[(df['teamPlacement'] <= 10) & (df['teamPlacement'] > 1)])
+        stats_dic['top_5_percent'] = 0.0
+        if stats_dic['top_5_count'] != 0:
+            stats_dic['top_5_percent'] = stats_dic['top_5_count'] / stats_dic['game_count']
+
+    for lst in lst_dic.keys():
+        col_lst = lst_dic[lst]
+        if lst == 'mu':
+            for col in col_lst:
+                stats_dic['average_' + col] = np.mean(df[col])
+            continue
+        elif lst == 'sum':
+            for col in col_lst:
+                stats_dic['total_' + col] = np.sum(df[col])
+            continue
+        elif lst == 'max':
+            for col in col_lst:
+                stats_dic['max_' + col] = np.max(df[col])
+
+    return {str(i): float(stats_dic[i]) for i in stats_dic.keys()}
+
+
+class Performance:
+
+    original_df: pd.DataFrame
+    map_choice: str
+    mode_choice: str
+    uno: str
+
+    def __init__(self, original_df, map_choice, mode_choice, uno):
+        self._map = map_choice
+        self._mode = mode_choice
+        self._stats = _get_stats(doc_filter=DocumentFilter(original_df=original_df,
+                                                           map_choice=map_choice,
+                                                           mode_choice=mode_choice,
+                                                           uno=uno))
+
+    def __repr__(self):
+        return self._mode
+
+    @property
+    def map(self) -> str:
+        return self._map
+
+    @property
+    def mode(self) -> str:
+        return self._mode
+
+    @property
+    def stats(self) -> dict:
+        return self._stats
+
+
+def _get_stats_per_map(map_choice: str, original_df: pd.DataFrame, uno: str) -> Dict[str, Performance]:
+    mode_lst = ['solo', 'duo', 'trio', 'quad']
+    return {_mode: Performance(original_df=original_df,
+                               map_choice=map_choice,
+                               mode_choice=_mode,
+                               uno=uno) for _mode in mode_lst}
+
 
 class Person:
 
-    # Whole data per gamer
-    gamertag: str = None
-    uno: str = None
-    total_kills: int = None
-    total_deaths: int = None
-    total_kd: float = None
-    total_headshot_ratio: float = None
-    total_games_played: int = None
-    total_play_time: int = None
-    match_ids: List[str] = None
-    whole_index: List[int] = None
-    match_rows: pd.DataFrame = None
+    original_df: pd.DataFrame
+    uno: str
+    gamertag: str
 
-    # Data for Rebirth
-    rebirth_wins: int = None
-    rebirth_top_5s: int = None
-    rebirth_win_percent: float = None
-    rebirth_top_5_percent: float = None
-    rebirth_game_count: int = None
-    rebirth_average_placement: float = None
-    rebirth_time_played: float = None
-    rebirth_kills: int = None
-    rebirth_deaths: int = None
-    rebirth_kd: float = None
-    rebirth_average_damage_done: float = None
-    rebirth_average_damage_taken: float = None
-    rebirth_match_ids: List[str] = None
+    def __init__(self, original_df, uno, gamertag):
+        self._uno = uno
+        self._gamertag = gamertag
+        self._rebirth_stats = _get_stats_per_map(map_choice='mp_e', original_df=original_df, uno=uno)
+        self._verdansk_stats = _get_stats_per_map(map_choice='mp_d', original_df=original_df, uno=uno)
 
-    # Data for Verdansk
-    verdansk_wins: int = None
-    verdansk_top_10s: int = None
-    verdansk_win_percent: float = None
-    verdansk_top_10_percent: float = None
-    verdansk_game_count: int = None
-    verdansk_average_placement: float = None
-    verdansk_time_played: float = None
-    verdansk_kills: int = None
-    verdansk_deaths: int = None
-    verdansk_kd: float = None
-    verdansk_average_damage_done: float = None
-    verdansk_average_damage_taken: float = None
-    verdansk_match_ids: List[str] = None
+    def __repr__(self):
+        return self.gamertag
 
-    kd_ratios: pd.DataFrame = None
-    weapons: pd.DataFrame = None
+    @property
+    def gamertag(self):
+        return self._gamertag
+
+    @property
+    def uno(self):
+        return self._uno
+
+    @property
+    def rebirth(self):
+        return self._rebirth_stats
+
+    @property
+    def verdansk(self):
+        return self._verdansk_stats
 
 
 class Squad:
 
-    # squad_lst: List[str] = None
-    # df: pd.DataFrame = None
-    # uno_name_dic: dict = None
+    squad_lst: List[str]
+    original_df: pd.DataFrame
+    uno_name_dic: Dict[str, str]
 
-    def __init__(self, squad_lst: List[str], df: pd.DataFrame, uno_name_dic: dict):
-        # self.squad_lst = squad_lst
-        # self.df = df
-        # self.uno_name_dic = uno_name_dic
-
-        squad_dic = {}
+    def __init__(self, squad_lst, original_df, uno_name_dic):
+        self._squad_stats = {teammate: Person(gamertag=teammate,
+                                              original_df=original_df,
+                                              uno=uno_name_dic[teammate]) for teammate in squad_lst}
+        lst = []
         for teammate in squad_lst:
-            temp_df = DocumentFilter(original_df=df, _uno=uno_name_dic[teammate]).get_df()
-            person = Person()
-            person.gamertag = teammate
-            person.uno = uno_name_dic[teammate]
-            person.total_kills = np.sum(temp_df['kills'])
-            person.total_deaths = np.sum(temp_df['deaths'])
-            person.total_kd = np.mean(temp_df['kdRatio'])
-            person.total_headshot_ratio = np.mean(temp_df['headshotRatio'])
-            person.total_games_played = len(temp_df)
-            person.total_play_time = np.sum(temp_df['timePlayed'])
-            person.match_ids = list(temp_df['matchId'].unique())
-            person.whole_index = list(temp_df.index)
+            for _mode in ['solo', 'duo', 'trio', 'quad']:
+                self._squad_stats[teammate].rebirth[_mode].stats['map'] = 'mp_e'
+                self._squad_stats[teammate].verdansk[_mode].stats['map'] = 'mp_d'
+                self._squad_stats[teammate].rebirth[_mode].stats['mode'] = _mode
+                self._squad_stats[teammate].verdansk[_mode].stats['mode'] = _mode
+                self._squad_stats[teammate].rebirth[_mode].stats['username'] = teammate
+                self._squad_stats[teammate].verdansk[_mode].stats['username'] = teammate
+                lst.append(self._squad_stats[teammate].rebirth[_mode].stats)
+                lst.append(self._squad_stats[teammate].verdansk[_mode].stats)
+        self._squad_df = pd.DataFrame(lst).fillna(0.0)
 
-            for _map in ['mp_d', 'mp_e']:
-                temp_df = DocumentFilter(original_df=df, _map=_map, _uno=person.uno).get_df()
+    def __repr__(self):
+        return 'Squad Stats'
 
-                if _map == 'mp_d':
-                    person.verdansk_kills = np.sum(temp_df['kills'])
-                    person.verdansk_deaths = np.sum(temp_df['deaths'])
-                    person.verdansk_kd = np.mean(temp_df['kdRatio'])
-                    person.verdansk_average_damage_done = np.mean(temp_df['damageDone'])
-                    person.verdansk_average_damage_taken = np.mean(temp_df['damageTaken'])
-                    person.verdansk_game_count = len(temp_df)
-                    person.verdansk_time_played = np.sum(temp_df['timePlayed'])
-                    person.verdansk_top_10s = len(temp_df[(temp_df['teamPlacement'] <= 10) & (temp_df['teamPlacement'] > 1)])
-                    person.verdansk_wins = len(temp_df[temp_df['teamPlacement'] == 1])
-                    person.verdansk_average_placement = np.mean(temp_df['placementPercent'])
-                    person.verdansk_match_ids = list(temp_df['matchID'].unique())
+    @property
+    def squad_dic(self) -> Dict[str, Person]:
+        return self._squad_stats
 
-                    person.verdansk_win_percent = 0.0
-                    if person.verdansk_wins != 0:
-                        person.verdansk_win_percent = person.verdansk_wins / person.verdansk_game_count
-
-                    person.verdansk_top_10_percent = 0.0
-                    if person.verdansk_top_10s != 0:
-                        person.verdansk_top_10_percent = person.verdansk_top_10s / person.verdansk_game_count
-
-                else:
-                    person.rebirth_kills = np.sum(temp_df['kills'])
-                    person.rebirth_deaths = np.sum(temp_df['deaths'])
-                    person.rebirth_kd = np.mean(temp_df['kdRatio'])
-                    person.rebirth_average_damage_done = np.mean(temp_df['damageDone'])
-                    person.rebirth_average_damage_taken = np.mean(temp_df['damageTaken'])
-                    person.rebirth_game_count = len(temp_df)
-                    person.rebirth_time_played = np.sum(temp_df['timePlayed'])
-                    person.rebirth_top_5s = len(temp_df[(temp_df['teamPlacement'] <= 5) & (temp_df['teamPlacement'] > 1)])
-                    person.rebirth_wins = len(temp_df[temp_df['teamPlacement'] == 1])
-                    person.rebirth_average_placement = np.mean(temp_df['placementPercent'])
-                    person.rebirth_match_ids = list(temp_df['matchID'].unique())
-
-                    person.rebirth_win_percent = 0.0
-                    if person.rebirth_wins != 0:
-                        person.rebirth_win_percent = person.rebirth_wins / person.rebirth_game_count
-
-                    person.rebirth_top_5_percent = 0.0
-                    if person.rebirth_top_5s != 0:
-                        person.rebirth_top_5_percent = person.rebirth_top_5s / person.rebirth_game_count
-
-            squad_dic[teammate] = person
-
-        self.squad_dic = squad_dic
-
-
+    @property
+    def squad_df(self) -> pd.DataFrame:
+        return self._squad_df
