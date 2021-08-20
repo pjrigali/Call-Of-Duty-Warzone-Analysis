ff8479bc98ba5bcd4658ea31d32295824977c24d
Peter <prigali>
2021-08-20 14:15:59 -0500

    updates to DocumentFilter class

--- Classes/document_filter.py
+++ Classes/document_filter.py
@@ -6,47 +6,77 @@ import pandas as pd
 @dataclass
 class DocumentFilter:
 
-    original_df: pd.DataFrame
-    _map: Optional[str]
-    _mode: Optional[str]
-    _username: Optional[str]
-    _uno: Optional[int]
-    _username_dict: Optional[dict]
-    _username_lst: Optional[List[str]]
-    data: pd.DataFrame
-
     def __init__(self,
                  original_df: pd.DataFrame,
-                 _map: str = None,
-                 _mode: str = None,
-                 _username: str = None,
-                 _uno: int = None,
-                 _username_dic: dict = None,
-                 _username_lst: List[str] = None):
-        self.original_df = original_df
-        self._map = _map
-        self._mode = _mode
-        self._username = _username
-        self._uno = _uno
-        self._username_dic = _username_dic
-        self._username_lst = _username_lst
+                 map_choice: Optional[str] = None,
+                 mode_choice: Optional[str] = None,
+                 username: Optional[str] = None,
+                 uno: Optional[int] = None,
+                 username_dic: Optional[dict] = None,
+                 username_lst: Optional[List[str]] = None,
+                 ):
 
         data = original_df.copy()
-        if _map:
-            data = data[data['map'] == _map]
+        if map_choice:
+            data = data[data['map'] == map_choice]
 
-        if _mode:
-            data = data[data['mode'] == _mode]
+        if mode_choice:
+            data = data[data['mode'] == mode_choice]
 
-        if _username:
-            data = data[data['uno'] == _username_dic[_username]]
+        if username:
+            if username_dic is None:
+                raise AttributeError('Need to pass a dict. Example {name or gamertag: uno number, ...}')
+            else:
+                data = data[data['uno'] == username_dic[username]]
 
-        if _uno:
-            data = data[data['uno'] == _uno]
+        if uno:
+            data = data[data['uno'] == uno]
 
-        if _username_lst:
-            u_lst = [_username_dic[i] for i in _username_lst]
-            data_lst = list(data['uno'])
-            data = data.iloc[[i for i, j in enumerate(data_lst) if str(j) in u_lst]]
+        if username_lst:
+            if username_dic is None:
+                raise AttributeError('Need to pass a dict. Example {name or gamertag: uno number, ...}')
+            else:
+                u_lst = [username_dic[i] for i in username_lst]
+                data_lst = list(data['uno'])
+                data = data.iloc[[i for i, j in enumerate(data_lst) if str(j) in u_lst]]
 
-        self.data: pd.DataFrame = data.sort_values('startDateTime', ascending=True).reset_index(drop=True)
+        self._df = data.sort_values('startDateTime', ascending=True).reset_index(drop=True)
+        self._unique_id_lst = list(self.df['matchID'].unique())
+        self._id_lst = list(self.df['matchID'])
+        self._map = map_choice
+        self._mode = mode_choice
+        self._username = username
+        self._uno = uno
+        self._username_lst = username_lst
+
+    @property
+    def df(self) -> pd.DataFrame:
+        return self._df
+
+    @property
+    def map_choice(self) -> Optional[str]:
+        return self._map
+
+    @property
+    def mode_choice(self) -> Optional[str]:
+        return self._mode
+
+    @property
+    def uno(self) -> Optional[str]:
+        return self._uno
+
+    @property
+    def username(self) -> Optional[str]:
+        return self._username
+
+    @property
+    def username_lst(self) -> Optional[List[str]]:
+        return self._username_lst
+
+    @property
+    def unique_ids(self) -> Optional[List[str]]:
+        return self._unique_id_lst
+
+    @property
+    def ids(self) -> Optional[List[str]]:
+        return self._id_lst
