e872e62e393e535d0f827ca75ba40a9e5eaf669b
Peter <prigali>
2021-08-21 00:54:18 -0500

    adds regression class

---
+++ Classes/regression.py
@@ -0,0 +1,73 @@
+from typing import Union, List
+import numpy as np
+from statsmodels import regression
+from statsmodels.tools import add_constant
+from Classes.documnent_filter import DocumentFilter
+
+
+class Regression:
+
+    doc_filter: DocumentFilter
+    x_column: Union[str, List[str]]
+    y_column: str
+
+    def __init__(self, doc_filter, x_column, y_column):
+
+        x = add_constant(np.array(doc_filter.df[x_column], dtype=float))
+        y = np.array(doc_filter.df[y_column])
+        model = regression.linear_model.OLS(y, x).fit()
+
+        self._r2 = model.rsquared
+        self._constant_coef = model.params[0]
+        self._item_coef = model.params[1]
+        self._lower_conf = model.conf_int()[1, :2][0]
+        self._upper_conf = model.conf_int()[1, :2][1]
+        self._pvalue = model.pvalues[1]
+        self._resid = model.resid
+        self._bse = model.bse
+        self._mse = model.mse_model
+        self._ssr = model.ssr
+        self._ess = model.ess
+
+    def __repr__(self):
+        return self._r2
+
+    @property
+    def r2(self):
+        return self._r2
+
+    @property
+    def constant_coefficient(self):
+        return self._constant_coef
+
+    @property
+    def x_coefficient(self):
+        return self._item_coef
+
+    @property
+    def lower_confidence(self):
+        return self._lower_conf
+
+    @property
+    def upper_confidence(self):
+        return self._upper_conf
+
+    @property
+    def pvalue(self):
+        return self._pvalue
+
+    @property
+    def residuals(self):
+        return self._resid
+
+    @property
+    def mse(self):
+        return self._mse
+
+    @property
+    def ssr(self):
+        return self._ssr
+
+    @property
+    def ess(self):
+        return self._ess
