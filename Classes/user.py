726135473f6706692e24eabb4de6ae989e6f3be4
Peter <prigali>
2021-08-25 04:09:48 -0500

    adds check to see if user gamertag in squad list

--- Classes/user.py
+++ Classes/user.py
@@ -101,6 +101,9 @@ class User:
         else:
             self.DRIVER_PATH = info['driverpath']
 
+        if self._gamertag not in self._squad:
+            self._squad = [self._gamertag] + self._squad
+
     def __repr__(self):
         return self.gamertag
 
