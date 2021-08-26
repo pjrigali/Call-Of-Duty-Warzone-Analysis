559690beabe668bfe234ebaee31798d63315beea
Peter <prigali>
2021-08-25 23:56:09 -0500

    add docs

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Call-Of-Duty-Warzone-Analysis'
+project = 'Warzone Analysis'
 copyright = '2021, Peter Rigali'
 author = 'Peter Rigali'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = 'v2.1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -39,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -1,10 +1,10 @@
-.. Call-Of-Duty-Warzone-Analysis documentation master file, created by
-   sphinx-quickstart on Sun Aug 15 08:05:44 2021.
+.. Warzone Analysis documentation master file, created by
+   sphinx-quickstart on Wed Aug 25 23:51:57 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Call-Of-Duty-Warzone-Analysis's documentation!
-=========================================================
+Welcome to Warzone Analysis's documentation!
+============================================
 
 .. toctree::
    :maxdepth: 2
