a025eb13472802367a3e0758190c8dde4cf7f876
Peter <prigali>
2021-08-26 04:10:07 -0500

    ReadtheDocs related

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='warzone',
+    version='v2.2.1',
+    packages=['Utils', 'Classes'],
+    package_dir={'': 'Call-Of-Duty-Warzone-Analysis/Classes'},
+    url='',
+    license='',
+    author='Peter',
+    author_email='peterjrigali@gmail.com',
+    description='Call of Duty Warzone Analysis'
+)
