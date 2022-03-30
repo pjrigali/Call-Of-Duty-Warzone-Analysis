e1dc65d81dff7e28bc7d31a5722aea2b414f177b
Peter <prigali>
2022-03-30 15:25:57 -0500

    Adds hacker list

--- warzone/utils/lists.py
+++ warzone/utils/lists.py
@@ -20,3 +20,11 @@ MAX_LST = ['headshots', 'kills', 'deaths', 'distanceTraveled', 'damageDone', 'da
            'objectiveBrDownEnemyCircle4', 'objectiveBrDownEnemyCircle5', 'objectiveBrDownEnemyCircle6', 'kdRatio',
            'headshotRatio']
 """A list of columns to compute the max for"""
+HACKER_LST = ['headshots', 'kills', 'deaths', 'kdRatio', 'scorePerMinute', 'distanceTraveled',
+              'objectiveBrKioskBuy', 'percentTimeMoving', 'longestStreak', 'damageDone', 'damageTaken',
+              'missionsComplete', 'objectiveLastStandKill', 'objectiveBrDownEnemyCircle1',
+              'objectiveBrDownEnemyCircle2', 'objectiveBrDownEnemyCircle3', 'objectiveBrDownEnemyCircle4',
+              'objectiveBrDownEnemyCircle5', 'objectiveBrDownEnemyCircle6', 'objectiveTeamWiped',
+              'objectiveReviver', 'headshotRatio', 'objectiveMunitionsBoxTeammateUsed',
+              'objectiveBrCacheOpen', 'objectiveMedalScoreKillSsRadarDrone']
+"""A list of columns used for detecting hackers"""
