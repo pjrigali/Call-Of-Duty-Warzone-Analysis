7d111d3e754e09625155104479262dcfe8a3b539
Peter <prigali>
2022-04-16 09:32:34 -0500

    adds documentation

--- warzone/classes/game.py
+++ warzone/classes/game.py
@@ -15,7 +15,7 @@ from warzone.utils.lists import SUM_LST
 @dataclass
 class Game:
     """Builds a single game"""
-    __slots__ = ('team', 'lobby', 'position', 'match_id')
+    __slots__ = ['team', 'lobby', 'position', 'match_id']
 
     def __init__(self, position: int, match_id: str):
         self.match_id = match_id
@@ -23,21 +23,28 @@ class Game:
         self.lobby = None
         self.position = position
 
-    def get_team_stat(self, data: pd.DataFrame, stat_type: str):
+    def add_team_stat(self, data: pd.DataFrame, stat_type: str):
+        """Add team stats"""
         stat = Stat(stat=stat_type, na='zero', dtype='float', empty=True)
         self.team = tuple([stat.get(data=data[col], q=None) for col in SUM_LST])
         return self
 
-    def get_lobby_stat(self, data: pd.DataFrame, stat_type: str):
+    def add_lobby_stat(self, data: pd.DataFrame, stat_type: str):
+        """Add lobby stats"""
         stat = Stat(stat=stat_type, na='zero', dtype='float', empty=True)
         self.lobby = tuple([stat.get(data=data[col], q=None) for col in SUM_LST])
         return self
 
-    def get_team_lobby_stat(self, team_data: pd.DataFrame, lobby_data: pd.DataFrame, stat_type: str):
+    def add_team_lobby_stat(self, team_data: pd.DataFrame, lobby_data: pd.DataFrame, stat_type: str):
+        """Add both team and lobby stats"""
         stat = Stat(stat=stat_type, na='zero', dtype='float', empty=True)
         self.team = tuple([stat.get(data=team_data[col], q=None) for col in SUM_LST])
         self.lobby = tuple([stat.get(data=lobby_data[col], q=None) for col in SUM_LST])
         return self
+
+    def get_dict(self):
+        """Return a dict"""
+        return {SUM_LST[ind]: val for ind, val in enumerate(self.team)}, {SUM_LST[ind]: val for ind, val in enumerate(self.lobby)}
 
     def __repr__(self):
         return 'Game ' + str(self.position)
